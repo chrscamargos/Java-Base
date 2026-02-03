da55d38e09a401178a1ffa88a7fa9fdcd14d8a70
chrscamargos <christianrodriguesdc2501@gmail.com>
2026-02-03 09:54:43 -0300

    aula de if e else

---
+++ src/aula3/IfElse.java
@@ -0,0 +1,14 @@
+package aula3;
+
+public class IfElse {
+    static void main (){
+        int idade = 18;
+
+        if (idade < 18){
+            IO.println("vc foi barrado");
+        }else {
+            IO.println("liberado");
+        }
+
+    }
+}
