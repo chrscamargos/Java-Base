55b96a43f24c3923d87ea948073383ef65ebca71
christian <christianrodriguesdc2501@gmail.com>
2026-04-09 10:54:23 -0300

    edição

--- aulas/Cache/src/Main.java
+++ aulas/Cache/src/Main.java
@@ -1,7 +1,6 @@
 
 void main() {
-//  List<Pessoa> banco: representa o banco de dados.
-// List<Pessoa> cache: representa os dados mais acessados recentemente.
+
     List<Pessoa> banco = new ArrayList<>();
     banco.add(new Pessoa(1, "Igor", 20));
     banco.add(new Pessoa(2, "Camily", 19));
@@ -10,25 +9,20 @@ void main() {
     banco.add(new Pessoa(5, "Guilherme", 17));
 
     List<Pessoa> cache = new ArrayList<>();
-//    Funcionalidade:
-//    Peça para o usuário digitar um ID de pessoa.
+
     cache.add(new Pessoa());
     Scanner sc = new Scanner(System.in);
     IO.println("Digite o ID");
     int id = sc.nextInt();
     boolean encontrada = false;
 
-//    Primeiro verifique se a pessoa já está no cache.
-//    Se estiver, imprima: Pessoa encontrada no cache: ...
     while (true) {
         for (Pessoa pessoa : cache) {
             if (id == pessoa.getId()) {
                 IO.println("Pessoa encontrada no cache: " + pessoa);
                 encontrada = true;
-                //    Se não estiver, busque no banco:
             }
         }
-//    Se encontrar, adicione ao cache e imprima: Pessoa buscada no banco e adicionada ao cache: ...
         if (!encontrada) {
             for (Pessoa pessoa : banco) {
                 if (id == pessoa.getId()) {
