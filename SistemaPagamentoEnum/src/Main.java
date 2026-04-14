750e84be85d7310be673d279dc776df41bb44d9f
christian <christianrodriguesdc2501@gmail.com>
2026-04-14 11:05:49 -0300

    atividade de Enumerador

---
+++ SistemaPagamentoEnum/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main(){
+    Pedido pedido = new Pedido(1, "daniel",StatusPedido.AGUARDANDO_PAGAMENTO);
+
+    pedido.avancarStatus(pedido);
+    IO.println(pedido.getStatus());
+
+    pedido.avancarStatus(pedido);
+    IO.println(pedido.getStatus());
+}
